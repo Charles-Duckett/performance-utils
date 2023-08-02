27fd41edb8da9614e937f46b424b26c6b2a00512
Charles-Duckett <duckettchase@gmail.com>
2023-08-02 10:52:25 -0400

    setup package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="my_package",
+    version="0.1",
+    author="Chase Duckett",
+    description="A package for getting the most out of my computer",
+    packages=find_packages(),
+    install_requires=[],
+)
