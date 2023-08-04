aa00d9d556b494851dd685cc5f0e6f718259149e
Charles-Duckett <duckettchase@gmail.com>
2023-08-03 20:30:56 -0400

    sestup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="my_package",
+    name="performance-utils",
     version="0.1",
     author="Chase Duckett",
     description="A package for getting the most out of my computer",
